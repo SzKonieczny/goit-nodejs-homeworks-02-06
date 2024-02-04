f654bde0898543e8bd5ad7b9a2abdec652159a58
Szymon Konieczny <szymon_konieczny@o2.pl>
2024-02-05 00:17:26 +0100

    services/auth -> sendEmail.js added

--- services/auth/sendEmail.js
+++ services/auth/sendEmail.js
@@ -1,23 +1,37 @@
-const sgEmail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 
-const { SENDGRID_API_KEY } = process.env;
+// const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD } = process.env;
 
-sgEmail.setApiKey(SENDGRID_API_KEY);
+// Konfiguracja transportera dla Nodemailer
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailgun.org",
+  port: 587,
+  secure: false, // Ustaw true, jeśli używa sie SSL
+  auth: {
+    user: "postmaster@sandboxb439229341e34405a14f0a8d1b53eea1.mailgun.org",
+    pass: "9cf98819607968146b5b9a52c0ef4f6d-69a6bd85-5321c6bb",
+  },
+});
 
 const sendEmail = async (email, verifyToken) => {
   console.log(email, verifyToken);
 
-  const mail = {
-    from: "s_kon@op.pl",
+  // Utwórz opcje wiadomości e-mail
+  const mailOptions = {
+    from: "postmaster@sandboxb439229341e34405a14f0a8d1b53eea1.mailgun.org",
     to: email,
     subject: "Verify email",
-    html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verifyToken}">Please, confirm your email</a>`,
+    text: "Please, confirm your email",
   };
 
-  await sgEmail
-    .send(mail)
-    .then(() => console.log("Email send success"))
-    .catch((error) => console.log(error.response));
+  // Wysylanie e-mail za pomocą transportera Nodemailer
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 };
 
 module.exports = sendEmail;
